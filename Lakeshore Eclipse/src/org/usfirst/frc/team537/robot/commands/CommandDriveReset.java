5c45ee317b7086504347e4b602f01b4807e992af
Aaron Phalphouvong <aaronphal@gmail.com>
2018-11-03 00:41:24 -0500

    sa

---
+++ Lakeshore Eclipse/src/org/usfirst/frc/team537/robot/commands/CommandDriveReset.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team537.robot.commands;
+
+import org.usfirst.frc.team537.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class CommandDriveReset extends Command {
+	public CommandDriveReset() {
+		requires(Robot.subsystemDrive);
+	}
+
+	@Override
+	protected void initialize() {
+	}
+
+	@Override
+	protected void execute() {
+		Robot.subsystemDrive.recalibrate();
+		Robot.subsystemGyro.reset();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return true;
+	}
+
+	@Override
+	protected void end() {
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+	}
+}
